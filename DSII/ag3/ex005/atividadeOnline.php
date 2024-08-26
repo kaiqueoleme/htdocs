4bc0c8d54b4ea5c762516a393bb8b7efa0ac95d7
kaiqueoleme <166898993+kaiqueoleme@users.noreply.github.com>
2024-08-26 01:55:09 -0300

    php

--- DSII/ag3/ex005/atividadeOnline.php
+++ DSII/ag3/ex005/atividadeOnline.php
@@ -3,17 +3,11 @@
 <html lang="pt-BR">
 
 <head>
-
     <meta charset="UTF-8">
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-
     <title>Atividade Online</title>
-
 </head>
 
 <body>
