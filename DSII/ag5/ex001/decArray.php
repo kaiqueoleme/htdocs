28e4efd2cebfcacc9718ac2dfb8913da14227896
kaiqueoleme <166898993+kaiqueoleme@users.noreply.github.com>
2024-09-08 23:30:30 -0300

    Update decArray.php

--- DSII/ag5/ex001/decArray.php
+++ DSII/ag5/ex001/decArray.php
@@ -1,10 +1,11 @@
-<?php 
-    /*
+<?php
+/*
     $sudeste = ["São Paulo", "Minas Gerais", "Rio de Janeiro", "Espírito Santo"];
 
     echo $sudeste[1];
     */
 
-    $filme = ['titulo' => 'Uma mente brilhante', 'Uma mente Brilhante', 'duracao' => '135min', 'genero' => 'drama'];
-    echo $filme['titulo'];
-?>
+$filme = ['titulo' => 'Uma mente brilhante', 'Uma mente Brilhante', 'duracao' => '135min', 'genero' => 'drama'];
+echo $filme['titulo'] . '<br>';
+echo $filme['duracao'] . '<br>';
+echo $filme['genero'] . '<br>';
