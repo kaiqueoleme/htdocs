be631ec687591e7a25718528ac1e85c9f4ff4da0
kaiqueoleme <166898993+kaiqueoleme@users.noreply.github.com>
2024-09-24 01:05:59 -0300

    Update lista.php

--- DSII/ag7/ex002/lista.php
+++ DSII/ag7/ex002/lista.php
@@ -18,12 +18,48 @@
     try {
 
         $conexao = new PDO($dsn, $user, $password);
+        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+    ?>
+        <div class="w3-margin w3-padding w3-content w3-half w3-display-topmiddle">
+            <h1 class="w3-center w3-margin w3-yellow w3-round-large">Lista de Estados</h1>
+            <table class="w3-table-all w3-centered">
+                <thead>
+                    <tr class="w3-center w3-green w3-text-white">
+                        <th>Código</th>
+                        <th>Nome</th>
+                        <th>Sigla</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                    <?php
+                    $sql = 'SELECT * FROM estado';
+                    $resultado  = $conexao->query($sql);
+                    if ($resultado != null) 
+                        foreach ($resultado as $lista) {
+                    ?>
+                        <tr class="">
+                            <td><?php $lista['idestado']; ?></td>
+                            <td><?php $lista['nome']; ?></td>
+                            <td><?php $lista['sigla']; ?></td>
+                        </tr>
+                    <?php
+                        };
+
+                    ?>
+                </tbody>
+            </table>
+
+        <?php
     } catch (PDOException $e) {
 
         echo ('Erro :' . $e->getMessage());
     }
-    $sql = ''
-    ?>
+        ?>
+        </div>
 
 </body>
 
