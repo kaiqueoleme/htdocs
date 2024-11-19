15c2d37ca3cb619057a6d89eb4fd89f0dbed3cf0
kaiqueoleme <166898993+kaiqueoleme@users.noreply.github.com>
2024-11-19 15:21:51 -0300

    Add switch

--- CursoEmVIdeo/ag10/ex03/ex03.php
+++ CursoEmVIdeo/ag10/ex03/ex03.php
@@ -9,8 +9,54 @@
 <body>
     <div>
         <?php
+            $e = isset($_GET['estado'])?$_GET['estado']:0;
 
+            switch ($e) {
+                case 1:
+                case 3:
+                case 4:
+                case 13:
+                case 21:
+                case 22:
+                case 26:
+                    $regiao = "Região Norte";
+                    break;
+                case 2:
+                case 5:
+                case 6:
+                case 9:
+                case 14:
+                case 16:
+                case 17:
+                case 19:
+                case 25:
+                    $regiao = "Região Nordeste";
+                    break;
+                case 7:
+                case 12:
+                case 18:
+                case 24:
+                    $regiao = "Região Sudeste";
+                    break;
+                case 8:
+                case 10:
+                case 11:
+                    $regiao = "Região Centro-Oeste";
+                    break;
+                case 15:
+                case 20:
+                case 23:
+                    $regiao = "Região Sul";
+                    break;
+                default:
+                    echo "Estado inválido";
+                    $regiao = "";
+            }
+            echo "Você mora na <span class='foco'>$regiao</span>";
         ?>
+        <br>
+        <a href="javascript:history.go(-1)"><button>Voltar</button></a>
+         
     </div>
 </body>
 </html>
