5b15da9e5e63b7ddab3d669fa72104378f37e95f
kaiqueoleme <166898993+kaiqueoleme@users.noreply.github.com>
2024-11-24 20:41:44 -0300

    add login

---
+++ NSTPHP/26-criptografia.php
@@ -0,0 +1,9 @@
+<?php
+$senha = "123456";
+$senha_db = "$2y$10$B0jcW3mT5B0FS0tWui5loe14SS9ktYop24TqzJ1uJopo0KA6/XTsu";
+
+if (password_verify($senha, $senha_db)) {
+    echo "Senha válida";
+} else  {"Senha Inválida"}
+$senhaSegura = password_hash($senha, PASSWORD_DEFAULT);
+echo $senhaSegura;
