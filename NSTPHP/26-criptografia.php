9f437e078f8a39c278a90f1c44ad2b657f080dac
kaiqueoleme <166898993+kaiqueoleme@users.noreply.github.com>
2024-11-24 20:55:30 -0300

    UP

--- NSTPHP/26-criptografia.php
+++ NSTPHP/26-criptografia.php
@@ -1,9 +1,10 @@
 <?php
 $senha = "123456";
-$senha_db = "$2y$10$B0jcW3mT5B0FS0tWui5loe14SS9ktYop24TqzJ1uJopo0KA6/XTsu";
+$senha_db = '$2y$10$B0jcW3mT5B0FS0tWui5loe14SS9ktYop24TqzJ1uJopo0KA6/XTsu';
 
 if (password_verify($senha, $senha_db)) {
-    echo "Senha válida";
-} else  {"Senha Inválida"}
-$senhaSegura = password_hash($senha, PASSWORD_DEFAULT);
-echo $senhaSegura;
+    echo "Senha válida <br>";
+} else {
+    echo "Senha Inválida";
+}
+ echo password_hash($senha, PASSWORD_DEFAULT);
